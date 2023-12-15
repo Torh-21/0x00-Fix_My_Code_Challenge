28ad433712004fbfc5c197fce9a2f8d3304d42e8
Torh-21 <majarotoheeb@gmail.com>
2023-12-15 14:08:46 -0800

    Fix check

---
+++ 0x01-Fix_My_Code_Challenge/status_server/api/v1/views/index.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python3
+""" Index view
+"""
+from flask import Blueprint, jsonify
+
+from api.v1.views import app_views
+
+# Creating a Blueprint instance to export the route
+app_views = Blueprint('app_views', __name__)
+
+# Status Route
+@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the web server
+    """
+    return jsonify({"status": "OK"})
+
